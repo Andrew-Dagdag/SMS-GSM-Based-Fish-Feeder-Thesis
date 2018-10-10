5b756689189c42bb690a5664c8c0a8abcfe99c83
AndrewDagdag <aedagdag1@up.edu.ph>
2018-10-10 10:28:49 +0800

    Almost finished with the field units, added more ajax

--- Server/server.js
+++ Server/server.js
@@ -12,9 +12,8 @@ let sha256      = require("sha256")
 let userdata = {}
 let feederdata = {}
 
-
 app.use(express.static(__dirname + "/html"));
-// app.use(express.static(__dirname + "/css"));
+app.use(express.static(__dirname + "/css"));
 // app.use(express.static(__dirname + "/js"));
 app.use(express.static(__dirname + "/bower_components"));
 
@@ -25,24 +24,58 @@ app.get('/', (request, response) => {
 });
 
 app.get("/FishFeedIndex", (request, response) => {
-  response.sendFile('fishfeedindex.html', {"root": "html/"})
+  response.sendFile('units.html', {"root": "html/"})
+});
+
+/*********************
+**AJAX POST REQUESTS**
+*********************/
+
+app.post('/updateSched', (request, response) => {
+  let fid = request.body.fid
+  let sched = request.body.fullSched
+  let amount = request.body.amount
+  let sql = "UPDATE `schedule` SET `amount`="+amount+", `sched`='"+sched+"' WHERE `schedule`.`fid`="+fid
+  console.log(sql)
+  con.query(sql, function(err, result){
+    if(err){
+      console.log(err)
+    }
+    response.json(result)
+  })
+});
+
+app.post('/getSched', (request, response) => {
+  let fid = request.body.fid
+  let sql = 'SELECT * from `schedule` WHERE fid='+fid
+  con.query(sql, function(err, result){
+    if(err){
+      console.log(err)
+    }
+    response.json(result)
+  })
+});
+
+app.post('/getFieldUnit', (request, response) => {
+  let fid = request.body.fid
+  let sql = 'SELECT * from `units` WHERE fid='+fid
+  con.query(sql, function(err, result){
+    if(err){
+      console.log(err)
+    }
+    response.json(result)
+  })
 });
 
 app.post('/getFieldUnits', (request, response) => {
   let uid = request.body.uid
   let sql = 'SELECT * FROM `units` WHERE uid='+uid
-  con.query(sql, function(err){
+  con.query(sql, function(err, result){
     if(err){
-      console.log(err)
+      // console.log(err)
     }
-    con.query(sql, function(err, result){
-      if(err){
-        console.log(err)
-      }
-      // console.log(result)
-      feederdata = result
-      response.json(feederdata)
-    })
+    feederdata = result
+    response.json(feederdata)
   })
 });
 
@@ -55,29 +88,23 @@ app.post('/userLogin', (request, response) => { //Ajax Request for Login
   let password = request.body.Pass
   let sql = 'SELECT * from `users` WHERE users.name="'+name+'"'
   // console.log("This is the query:", sql)
-  con.connect(function(err){
+  con.query(sql, function(err, result){
     if(err){
       console.log(err)
     }
-    // console.log("Connected to database! Checking user login info!")
-    con.query(sql, function(err, result){
-      if(err){
-        console.log(err)
-      }
-      // console.log(result[0])
-      let hashedPass = sha256(password)
-      let data = {}
-      data["success"] = false
-      if(hashedPass == result[0].password){
-        // console.log("Ayooo it passed lmao", hashedPass)
-        // let uid = result[0].uid
-        data["success"] = true
-        userdata["user"] = result[0].name
-        userdata["uid"] = result[0].uid
-        userdata["user_phone"] = result[0].phoneno
-      }
-      response.json(data)
-    })
+    // console.log(result[0])
+    let hashedPass = sha256(password)
+    let data = {}
+    data["success"] = false
+    if(hashedPass == result[0].password){
+      // console.log("Ayooo it passed lmao", hashedPass)
+      // let uid = result[0].uid
+      data["success"] = true
+      userdata["user"] = result[0].name
+      userdata["uid"] = result[0].uid
+      userdata["user_phone"] = result[0].phoneno
+    }
+    response.json(data)
   })
 });
 
